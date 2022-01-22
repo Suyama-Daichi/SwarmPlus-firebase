42b6c2050b3e8eadd515284a54f8a72ec4f3efd7
daichi.suyama <symdit@gmail.com>
2022-01-23 00:46:58 +0900

    feat: カスタムアクセストークンを作成する関数

--- functions/src/auth/genCustomToken.ts
+++ functions/src/auth/genCustomToken.ts
@@ -1,5 +1,14 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-exports.genCustomToken = functions.https.onRequest((request, response) => {
-  console.log("called");
+exports.genCustomToken = functions.https.onCall(async (request, response) => {
+  const uid = "some-uid";
+  const customToken = await admin
+    .auth()
+    .createCustomToken(uid)
+    .catch((error) => {
+      console.log("Error creating custom token:", error);
+    });
+
+  return { customToken };
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,6 @@
+import * as admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 exports.auth = require("./auth/genCustomToken");
