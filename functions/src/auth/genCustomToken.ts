9f91fb97cca289ce06f959acc53cf69063b610ac
daichi.suyama <symdit@gmail.com>
2022-01-23 11:14:36 +0900

    feat: カスタムアクセストークンをuidから作成する関数

--- functions/src/auth/genCustomToken.ts
+++ functions/src/auth/genCustomToken.ts
@@ -1,8 +1,8 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-exports.genCustomToken = functions.https.onCall(async (request, response) => {
-  const uid = "some-uid";
+exports.genCustomToken = functions.https.onCall(async (data) => {
+  const uid = data.uid;
   const customToken = await admin
     .auth()
     .createCustomToken(uid)
